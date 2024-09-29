3937a643527a09c87fb78b25304cef23c02c713c
hasibul.hasan <hasibul.hasan@technonext.com>
2024-09-29 13:09:03 +0600

    2 page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'employee-list',
+        component: EmployeeListComponent
+    },
+    {
+        path: 'add-employee',
+        component: AddEmployeeComponent
+    }
+];
--- src/app/components/employee-list/employee-list.component.ts
+++ src/app/components/employee-list/employee-list.component.ts
@@ -1,12 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { AddEmployeeComponent } from "../add-employee/add-employee.component";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, AddEmployeeComponent],
   templateUrl: './employee-list.component.html',
   styleUrl: './employee-list.component.css'
 })
 export class EmployeeListComponent {
+  constructor (private router:Router){
 
+  }
+
+  emplist = [
+    {id:1, name:'Hasibul Hasan',degination:'Jr.Software Engineer',phone:'01747979703'},
+    {id:2, name:'Hasibul Hasan',degination:'Jr.Software Engineer',phone:'01747979703'},
+    {id:3, name:'Hasibul Hasan',degination:'Jr.Software Engineer',phone:'01747979703'},
+    {id:4, name:'Hasibul Hasan',degination:'Jr.Software Engineer',phone:'01747979703'},
+    {id:5, name:'Hasibul Hasan',degination:'Jr.Software Engineer',phone:'01747979703'},
+    {id:6, name:'Hasibul Hasan',degination:'Jr.Software Engineer',phone:'01747979703'},
+    {id:7, name:'Hasibul Hasan',degination:'Jr.Software Engineer',phone:'01747979703'},
+    {id:8, name:'Hasibul Hasan',degination:'Jr.Software Engineer',phone:'01747979703'},
+  ];
+
+  editEmployee(){
+    this.router.navigateByUrl('add-employee');
+  }
 }
